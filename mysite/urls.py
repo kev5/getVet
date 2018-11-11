e326fa0932d6c0c156383b11f49d5a787e5820f6
kev5 <kevalkhara95@gmail.com>
2018-11-10 22:52:37 -0500

    static files update

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include 
 from django.conf import settings
-from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from mysite.vetapp import views
 
 urlpatterns = [
@@ -10,4 +10,4 @@ urlpatterns = [
 	path('profile/', views.profile, name='profile'),
 	path('accounts/', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
