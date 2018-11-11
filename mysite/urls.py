99aae2fc700cf3d3bc0c8a0b1dae92a727c95c00
kev5 <kevalkhara95@gmail.com>
2018-11-11 06:24:13 -0500

    adding profile/categories/companies

--- mysite/urls.py
+++ mysite/urls.py
@@ -6,8 +6,17 @@ from mysite.vetapp import views
 
 urlpatterns = [
 	path('', views.home, name='home'),
+	path('companies/', views.companies, name='companies'),
 	path('signup/', views.signup, name='signup'),
 	path('profile/', views.profile, name='profile'),
+	path('profile/', views.profile, name='read_more'),
+	path('profile/editprofile/', views.editprofile, name='editprofile'),
+	path('devprofile/editprofile/', views.editprofile, name='editprofile'),
+	path('techprofile/', views.technology, name='technology'),
+	path('custprofile/', views.customer, name='customer'),
+	path('hr/', views.hr, name='hr'),
+	path('admi/', views.admi, name='admi'),
+	path('devprofile/', views.dev_profile, name='devprofile'),
 	path('accounts/', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
 ]
--- mysite/vetapp/views.py
+++ mysite/vetapp/views.py
@@ -11,6 +11,10 @@ def home(request):
 	})
 
 
+def companies(request):
+	return render(request, 'companies.html')
+
+
 def signup(request):
 	if request.method == "POST":
 		form = UserCreationForm(request.POST)
@@ -29,3 +33,38 @@ def signup(request):
 def profile(request):
 	return render(request, 'profile.html')
 
+
+def read_more(request):
+	return render(request, 'profile.html')
+
+
+def dev_profile(request):
+	return render(request, 'devprofile.html')
+
+
+def technology(request):
+	return render(request, 'technology.html')
+
+
+def customer(request):
+	return render(request, 'customer.html')
+
+
+def hr(request):
+	return render(request, 'hr.html')
+
+
+def admi(request):
+	return render(request, 'admi.html')
+
+
+def editprofile(request):
+    if request.method == 'POST':
+        if request.POST.get('headline') and request.POST.get('content'):
+            post=Post()
+            post.title= request.POST.get('title')
+            post.content= request.POST.get('content')
+            post.save()
+            return render(request, 'editprofile.html')  
+    else:
+            return render(request,'editprofile.html')
